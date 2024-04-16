bf67a41f1165beb22076f481a9d695c575ff9128
Ramakrishna V <113897207+RamakrishnaVummadisetty@users.noreply.github.com>
2024-04-16 12:50:50 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
+	@GetMapping
+	public String welcome(){
+		return "github actions demo!";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
